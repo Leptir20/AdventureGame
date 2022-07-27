33f6ebe59b94942a43bf3b2681941ad0572dee3a
ANTCUR <71789931+Leptir20@users.noreply.github.com>
2022-07-27 14:46:12 -0400

    Create monster.py

---
+++ monster.py
@@ -0,0 +1,13 @@
+class Enemy: 
+
+    def monsterName(monName, name):
+        monName = name
+        return monName
+
+    def monsterHealth(monHealth, num1):
+        monHealth = num1
+        return monHealth
+    
+    def monsterAttack(monAttack, num2):
+        monAttack = num2
+        return monAttack
