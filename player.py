38cfd67534b9735873c46e0d86696ab09c6514cb
ANTCUR <71789931+Leptir20@users.noreply.github.com>
2022-07-27 14:46:28 -0400

    Create player.py

---
+++ player.py
@@ -0,0 +1,15 @@
+import random
+
+def playerName():
+    name = input("Please enter your name: ")
+    return name
+
+def playerHealth():
+    health = 10
+    health = str(health)
+    return health
+
+def playerAttack():
+    attack = 1
+    attack = str(attack)
+    return attack
