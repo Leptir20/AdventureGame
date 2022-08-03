db98f3dd01acbbe81f97a97e89a77dac46bdafdd
ANTCUR <71789931+Leptir20@users.noreply.github.com>
2022-08-03 15:39:10 -0400

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
-import random
+coins = 0
+health = 10
 
 def playerName():
     name = input("Please enter your name: ")
@@ -10,6 +11,30 @@ def playerHealth():
     return health
 
 def playerAttack():
-    attack = 1
+    attack = 5
     attack = str(attack)
     return attack
+
+def playerCoins():
+
+    global coins
+    import monster
+    monNum = monster.monsterNumber()
+
+    if monNum == 1:
+        coins = int(coins) + 5
+    if monNum == 2:
+        coins = int(coins) + 10
+    if monNum == 3:
+        coins = int(coins) + 15
+    return coins
+
+def updateHealth():
+    global health
+    health = health + 5
+    return health
+
+def coinsHealth():
+    global coins
+    coins = int(coins) - 20
+    return coins
