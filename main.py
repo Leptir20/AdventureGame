b7f1d56f091805f4b10d591b510ea852c0a9ec50
ANTCUR <71789931+Leptir20@users.noreply.github.com>
2022-08-03 15:38:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,92 @@
+import importlib
 import player
-import monster
-import random
 
+fight = "Yes"
 dead = 0
+shop = "No"
 
 health = player.playerHealth()
 attack = player.playerAttack()
 name = player.playerName()
-
-monster = monster.Enemy()
-monHealth = monster.monsterHealth(20)
-monName = monster.monsterName("John")
-
-monHealth = str(monHealth)
-
 print()
+
 print("Name: " + name)
 print("Health: " + health)
 print("Attack: " + attack)
 
-print()
-print("Monster Name: " + monName)
-print("Monster Health: " + monHealth)
+while fight != "No":
 
+    import monster
+    coins = player.playerCoins()
 
-while monHealth != dead:
+    monHealth = monster.monsterHealth()
+    monName = monster.monsterName()
+    monNum = monster.monsterNumber()
+    monAttack = monster.monsterAttack()
 
-    choice = input("Do you want to attack? Enter 'Yes' or 'No': ")
+    monHealth = str(monHealth)
 
-    if choice == "Yes" or choice == "yes":
-        monHealth = int(monHealth)
-        attack = int(attack)
-        monHealth = monHealth - attack
-        print()
+    print()
+    print("Monster Name: " + monName)
+    print("Monster Health: " + monHealth)
+    print("Monster Attack: " + str(monAttack))
+    print()
+
+    while monHealth != dead:
+
+        choice = input("Do you want to attack? Enter 'Yes' or 'No': ")
+
+        if choice == "Yes" or choice == "yes":
+            monHealth = int(monHealth)
+            attack = int(attack)
+            monHealth = monHealth - attack
+            print()
         
-        if monHealth >= dead:
-           print(monName + " health is at " + str(monHealth))
+            if monHealth > dead:
+               print(monName + " health is at " + str(monHealth))
 
-           monAttack = monster.monsterAttack(random.randint(2, 5))
-           health = int(health) - monAttack
-           print("The monster hit you for " + str(monAttack))
+               health = int(health) - int(monAttack)
+               print("The monster hit you for " + str(monAttack))
+               
 
-           if health <= dead:
-               choice = "No"
-               health = dead
+               if health <= dead:
+                   choice = "No"
+                   fight = "No"
+                   health = dead
 
-           print("Your health is at " + str(health))
-           print()
+               print("Your health is at " + str(health))
+               print()
 
-        elif monHealth <= dead:
-            print("You have vanquished the monster")
-            monHealth = dead
+            elif monHealth <= dead:
+                print("You have vanquished the monster")
+                shop = ""
+                monHealth = dead
 
 
 
-    if choice == "No" or choice == "no":
-        print("You have been eaten")
-        print("Game Over")
-        health <= dead
-        monHealth = health
+        if choice == "No" or choice == "no":
+            print()
+            print("You have been eaten")
+            print("Game Over")
+            health = dead
+            monHealth = health
+            fight = "No"
 
-    else: 
-        print("Please enter in 'Yes' or 'No': ")
+        while shop != "No":
+            if fight == "No":
+                break
+
+            print("You have " + str(coins) + " coins")
+            shop = input("Do you want to shop? ")
+
+            if shop != "No":
+                upgrade = input("What do you want to upgrade? ")
+                if upgrade == "Health":
+                    if coins >= 20:
+                        health = player.updateHealth()
+                        coins = player.coinsHealth()
+                    elif coins < 20:
+                        print("You do not have enough coins!")
+
+
+        importlib.reload(monster)
