220b4b900b64c267ece18300543f1afa001b6bea
ANTCUR <71789931+Leptir20@users.noreply.github.com>
2022-08-03 15:38:51 -0400

    Update monster.py

--- monster.py
+++ monster.py
@@ -1,13 +1,51 @@
-class Enemy: 
+import random
 
-    def monsterName(monName, name):
-        monName = name
-        return monName
+number = 1
 
-    def monsterHealth(monHealth, num1):
-        monHealth = num1
-        return monHealth
-    
-    def monsterAttack(monAttack, num2):
-        monAttack = num2
-        return monAttack
+def monsterName():
+    if randomChoice == 1:
+        monsterName = "John"
+    if randomChoice == 2:
+        monsterName = "Jim"
+    if randomChoice == 3:
+        monsterName = "Joe"
+    return monsterName
+
+def monsterHealth():
+    if randomChoice == 1:
+        monsterHealth = 10
+    if randomChoice == 2:
+        monsterHealth = 10
+    if randomChoice == 3:
+        monsterHealth = 10
+    return monsterHealth
+
+def monsterAttack():
+    if randomChoice == 1:
+        monsterAttack = 2
+    if randomChoice == 2:
+        monsterAttack = 2
+    if randomChoice == 3:
+        monsterAttack = 2
+    return monsterAttack
+
+def monsterNumber():
+    if randomChoice == 1:
+        monsterNumber = 1
+    if randomChoice == 2:
+        monsterNumber = 2
+    if randomChoice == 3:
+        monsterNumber = 3
+    return monsterNumber
+
+def randomMonster():
+    randomChoice = ""
+    randomChoice = random.randint(1,3)
+    return randomChoice
+
+while number == 1:
+    randomChoice = randomMonster()
+    monsterName()
+    monsterHealth()
+    monsterAttack()
+    break
